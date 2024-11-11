3d3c54a8ea378b773388bacce8e9e85d95b72af5
MarcoCAVS18 <90146299+MarcoCAVS18@users.noreply.github.com>
2024-11-11 00:59:17 +0000

    tumama

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-// App.js
 import React, { useState } from 'react';
 import DayInput from './components/DayInput';
 import CalculateButton from './components/CalculateButton';
 import List from './components/List';
 import Logo from './components/Logo';
-import './index.css'
+import './index.css';
 
 function App() {
   const [hours, setHours] = useState(Array(7).fill(0));
   const [showList, setShowList] = useState(false);
-  const [canCalculate, setCanCalculate] = useState(false); // Estado para habilitar/deshabilitar el botón
-  const [shake, setShake] = useState(false); // Estado para la animación de shake
+  const [canCalculate, setCanCalculate] = useState(false);
+  const [shake, setShake] = useState(false);
+
+  // Nueva constante para las horas ajustadas con el descuento de media hora
+  const adjustedHours = hours.map(hour => (hour > 5 ? hour - 0.5 : hour));
 
   const handleHourChange = (index, value) => {
     const updatedHours = [...hours];
@@ -25,8 +27,8 @@ function App() {
     if (canCalculate) {
       setShowList(true);
     } else {
-      setShake(true); // Activa la animación de shake
-      setTimeout(() => setShake(false), 500); // Resetea el shake después de 500ms
+      setShake(true);
+      setTimeout(() => setShake(false), 500);
     }
   };
 
@@ -36,23 +38,27 @@ function App() {
 
       <div className="w-full max-w-md bg-bgGray p-6 rounded-xl shadow-md">
         {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day, index) => (
-          <DayInput
-            key={index}
-            day={day}
-            value={hours[index]}
-            onChange={(value) => handleHourChange(index, value)}
-          />
+          <div key={day} className="mb-4">
+            <DayInput
+              day={day}
+              value={hours[index]}
+              onChange={(value) => handleHourChange(index, value)}
+            />
+          </div>
         ))}
+        
+        {/* Mensaje de nota debajo de todos los inputs */}
+        <p className="text-xs text-gray-500 mt-4">
+          * Recuerda colocar el total de horas; se descontará media hora automáticamente.
+        </p>
       </div>
 
-      {/* Botón Calculate usando el componente CalculateButton */}
       <CalculateButton
         onClick={calculateTotalHours}
         disabled={!canCalculate}
         shake={shake}
       />
-
-      {showList && <List hours={hours} />}
+      {showList && <List hours={adjustedHours} />} {/* Pasamos las horas ajustadas */}
     </div>
   );
 }
